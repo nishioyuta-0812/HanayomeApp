1119a2ea1e76fced876c337fc042b7d197243308
nishioyuta-0812 <yutanishi0812@gmail.com>
2022-01-02 09:29:41 +0900

    Initial Commit

---
+++ HanayomeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HanayomeApp
+//
+//  Created by 西尾悠太 on 2022/01/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HanayomeAppUITests/HanayomeAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HanayomeAppUITestsLaunchTests.swift
+//  HanayomeAppUITests
+//
+//  Created by 西尾悠太 on 2022/01/02.
+//
+
+import XCTest
+
+class HanayomeAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
